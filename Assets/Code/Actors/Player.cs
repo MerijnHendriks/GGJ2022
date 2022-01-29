010c5e21e35e9f4cf8fa656fcc8dd1ba99fe5db6
ShaquilleLouisa <s.louisa@home.nl>
2022-01-29 17:19:21 +0100

    Added Timer Updated Player

--- Assets/Code/Actors/Player.cs
+++ Assets/Code/Actors/Player.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Player : Actor
 {
     [SerializeField]
+    private Transform model;
+    [SerializeField]
     private Rigidbody2D rigidBody;
-    private float moveSpeed = 500;
+    private const float moveSpeed = 2000;
+    private const float jumpSpeed = 2500;
+    private Vector2 position;
+    private Timer timer = new Timer();
+    private bool isJumping;
 
     protected override void Start()
     {
@@ -15,16 +21,54 @@ public class Player : Actor
 
     private void Update()
     {
+        position = new Vector2(transform.position.x, transform.position.y);
         CheckInput();
+
+        //For Testing
+        if (InputController.IsPressed("RESET"))
+            transform.position = Vector3.zero;
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(position, Vector2.down, Color.green);
+        if (Physics2D.Raycast(position + Vector2.right * 0.5f, Vector2.down, 1.1f))
+            return true;
+        if (Physics2D.Raycast(position + Vector2.left * 0.5f, Vector2.down, 1.1f))
+            return true;
+        return false;
     }
 
     private void CheckInput()
     {
         int x = 0;
+
         if (InputController.IsPressed("MOVE_RIGHT"))
+        {
             x = 1;
+            model.transform.rotation = Quaternion.identity;
+        }
         if (InputController.IsPressed("MOVE_LEFT"))
+        {
             x = -1;
-        rigidBody.velocity = new Vector2(x * moveSpeed * Time.deltaTime, 0);
+            model.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (InputController.IsPressed("JUMP") && !isJumping && IsGrounded())
+            StartCoroutine(Jump());
+
+        rigidBody.velocity = new Vector2(x * moveSpeed * Time.deltaTime, position.y);
+    }
+
+    private IEnumerator Jump()
+    {
+        isJumping = true;
+        timer.Set(0.3f);
+        while (!timer.Done())
+        {
+            rigidBody.velocity += Vector2.up * jumpSpeed * Time.deltaTime;
+            yield return null;
+        }
+        isJumping = false;
     }
 }
---
+++ Assets/Code/Utility/Timer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class Timer
+{
+    //The time stamp where the timer starts
+    private float timeStamp;
+
+    //The interval of the timer
+    private float interval;
+
+    //The time that still is left when the timer gets paused
+    private float pauseDifference;
+
+    //The pause state of the timer
+    public bool isPaused { get; private set; }
+
+    //Set the Timer when creating the time
+    //public Timer(float interval)
+    //{
+    //    Set(interval);
+    //}
+
+    //Set the timer
+    public void Set(float interval = 2)
+    {
+        //Set the current timestamp
+        timeStamp = Time.time;
+        //Set the interval
+        this.interval = interval;
+    }
+
+    //Reset the timer
+    public void Reset()
+    {
+        //Set the timer with the last interval to reset it
+        Set(interval);
+    }
+
+    //Pause or Unpause the timer
+    public void Pause(bool pause)
+    {
+        if (pause)
+        {
+            //Save time (to continue after unpause)
+            pauseDifference = TimeLeft();
+            isPaused = pause;
+            //Set the pause state of the timer
+            return;
+        }
+
+        //Unpause the timer
+        isPaused = pause;
+        timeStamp = Time.time - (interval - pauseDifference);
+    }
+
+    //Returns how long the timer still needs to run
+    public float TimeLeft()
+    {
+        //Return the time that is left
+        return Done() ? 0 : (1 - Progress()) * interval;
+    }
+
+    //Get the progress of the timer
+    public float Progress()
+    {
+        //Calculate the progress of the timer
+        return (isPaused) ? (interval - pauseDifference) / interval : Done() == true ? 1 : Mathf.Abs((timeStamp - Time.time) / interval);
+    }
+
+    //Check if the timer is done
+    public bool Done()
+    {
+        //Return the status
+        return (isPaused) ? pauseDifference == 0.0f : Time.time >= timeStamp + interval ? true : false;
+    }
+}
